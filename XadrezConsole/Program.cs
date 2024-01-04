06432d91f79b98cc584e8398a1c778984d2e8534
Ulisses Neto <ulissesnf22@gmail.com>
2024-01-03 21:25:52 -0300

    Criando a classe posicao

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using Tabuleiro;
+
+namespace XadrezConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " + P);
+
+            Console.ReadLine();
+
+        }
+    }
+}
